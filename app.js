80f53408b222698f67e3bd141fb52b0cf3674eaa
Sumikaizen <sumikaizen@gmail.com>
2026-06-17 01:28:58 +0545

    Updated app to version 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('Hello from CI/CD Pipeline!\n');
+  res.end('Hello from CI/CD Pipeline - Version 2!\n');
 }).listen(3000);
 
 console.log('App running on port 3000');
